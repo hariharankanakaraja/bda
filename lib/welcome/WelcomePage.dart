8a3c20904127b8b10f8c71da0afbf9db9d00a74c
Hariharan Kanakaraja <hariharankanakaraja@gmail.com>
2024-04-21 12:05:27 +0530

    image

--- lib/welcome/WelcomePage.dart
+++ lib/welcome/WelcomePage.dart
@@ -22,10 +22,40 @@ class _WelcomePageState extends State<WelcomePage> {
             margin: EdgeInsets.symmetric(vertical: 10),
             color: Colors.blue,
             alignment: Alignment.center,
-            child: Text(
-              "Single Child Container",
-              style: TextStyle(fontSize: 30, color: Colors.white),
-            ),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 20),
+                    child: Text(
+                      "Blood Champ",
+                      style: TextStyle(fontSize: 30, color: Colors.white),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 20),
+                    child: Container(
+                      width: 200,
+                      height: 200,
+                      color: Colors.white,
+                      child: Image.asset("assets/images/bda1.png"),
+                    ),
+                  ),
+                  Text(
+                    "Welcome to Bload donation champ application",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        backgroundColor: Colors.red),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20),
+                    child: ElevatedButton(
+                        onPressed: () {}, child: Text("Let's Start")),
+                  )
+                ]),
           ),
         )),
       ),
